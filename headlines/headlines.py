82ee1db3ee1e32a0d668572ede12721f20648e29
geek7710 <bonilla7710@gmail.com>
2018-05-02 08:01:32 -0400

    adding "from flask import request", modified function get_news()

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,11 +11,15 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route("/<publication>")
 def get_news(publication="bbc"):
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return render_template("home.html",article=first_article)
+    return render_template("home.html",articles=feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
